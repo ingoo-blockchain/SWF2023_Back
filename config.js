cfacc2ed0eaf7222ddfdfd42af3211fdd7cbfa7f
ingoo <web7722@gmail.com>
2023-07-31 22:03:24 +0900

    fix: config.js 수정

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const { NODE_ENV } = process.env
-const isDev = NODE_ENV === 'production'
+const isDev = NODE_ENV === 'development'
 const SERVER_POST = isDev ? '3005' : '80'
 
 const baseURL = isDev ? 'http://127.0.0.1:4000' : 'https://api.develrocket.com'
@@ -14,6 +14,7 @@ const DATABASE = isDev ? 'LTL' : process.env.DB_DATABASE
 const s3url = `https://develrocket-test.s3.ap-northeast-2.amazonaws.com`
 
 module.exports = {
+    SERVER_POST,
     isDev,
     baseURL,
     DB_HOST,

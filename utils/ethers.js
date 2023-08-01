fad1e3caa36b03d4d3ec2540c71911832afc0b05
bitkunst <bitkunst23@gmail.com>
2023-08-01 15:27:57 +0900

    feat: queued and execute functions

--- utils/ethers.js
+++ utils/ethers.js
@@ -6,6 +6,7 @@ const governorABI = require('../contracts/GovernorContract.json').abi
 const governorCA = process.env.GOERLI_GOVERNOR
 const postingABI = require('../contracts/Posting.json').abi
 const postingCA = process.env.GOERLI_POSTING
+const STORE = 'store'
 
 const network = 'goerli'
 const provider = new ethers.providers.AlchemyProvider(network, process.env.ALCHEMY_GOERLI_API_KEY)
@@ -33,6 +34,39 @@ const transferGovernanceToken = async (signer, to, amount) => {
     await governanceToken.connect(signer).transfer(to, toWei(amount))
 }
 
+const getProposalState = async (proposalId) => {
+    const proposalState = await governor.state(proposalId)
+    // 0:Pending, 1:Active, 2:Canceled, 3:Defeated, 4:Succeeded, 5:Queued, 6:Expired, 7:Executed
+    return proposalState
+}
+
+const getProposalVotes = async (proposalId) => {
+    const proposalVotes = await governor.proposalVotes(proposalId)
+    return {
+        against: proposalVotes[0],
+        for: proposalVotes[1],
+    }
+}
+
+const queueAndExecuteProposal = async (signer, key, value) => {
+    const encodedFunctionCall = posting.interface.encodeFunctionData(STORE, [key, value])
+    const descriptionHash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(''))
+    const queueTx = await governor.connect(signer).queue([posting.address], [0], [encodedFunctionCall], descriptionHash)
+    await queueTx.wait(1)
+
+    setTimeout(async () => {
+        const executeTx = await governor
+            .connect(signer)
+            .execute([posting.address], [0], [encodedFunctionCall], descriptionHash)
+        await executeTx.wait(1)
+    }, 7000)
+}
+
+const retrievePosting = async (key) => {
+    const ipfsUrl = await posting.retrieve(key)
+    return ipfsUrl
+}
+
 module.exports = {
     provider,
     toWei,
@@ -40,4 +74,8 @@ module.exports = {
     createSigner,
     balanceOfGovernanceToken,
     transferGovernanceToken,
+    getProposalState,
+    getProposalVotes,
+    queueAndExecuteProposal,
+    retrievePosting,
 }

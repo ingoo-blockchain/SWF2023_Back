a23ab0fd4ac7b3434f0d4c94c11acfedfb57ee4f
ingoo <web7722@gmail.com>
2023-08-01 16:23:37 +0900

    fix: cors 처리

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ governor.on('ProposalExecuted', (profosalId) => {
     Proposal.updateState(profosalId)
 })
 
-app.use(cors())
+app.use(
+    cors({
+        credentials: true,
+    }),
+)
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
 
